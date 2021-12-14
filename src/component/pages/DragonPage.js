3ea3196bcd241286cbca5aa97438195456b98c96
unknown <okpara_tochukwu@yahoo.com>
2021-12-14 07:12:57 +0100

    fetched dragon

--- src/component/pages/DragonPage.js
+++ src/component/pages/DragonPage.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchDragon } from '../../redux/dragon/dragon';
 
-const DragonPage = () => (
-  <div>
-    <h3>DragonPage</h3>
-  </div>
-);
+const DragonPage = () => {
+  const dispatch = useDispatch();
+  const dragonDetail = useSelector((state) => state.dragonReducer);
+  useEffect(() => {
+    dispatch(fetchDragon());
+  }, []);
+  return (
+    <div>
+      {dragonDetail.map((dragonDetail) => (
+        <div key={dragonDetail.id}>
+          <h3>{dragonDetail.name}</h3>
+          <p1>{dragonDetail.type}</p1>
+        </div>
+      ))}
+    </div>
+  );
+};
 export default DragonPage;

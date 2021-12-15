7ef215d5c6d13ed7d4541ad5e566b4971b4a150c
MCKENT05 <atemitope95@gmail.com>
2021-12-15 13:44:16 +0100

    finished testing for rocketpage

--- src/__tests__/RocketPage.test.js
+++ src/__tests__/RocketPage.test.js
@@ -3,7 +3,7 @@ import { render, unmountComponentAtNode } from 'react-dom';
 import { act } from 'react-dom/test-utils';
 import pretty from 'pretty';
 import { Provider } from 'react-redux';
-import store from '../redux/configureStore'
+import store from '../redux/configureStore';
 import RocketPage from '../component/pages/rocketpage/RocketPage';
 
 let container = null;
@@ -23,9 +23,13 @@ afterEach(() => {
 describe('testing the app', () => {
   it('testing RocketPage UI', () => {
     act(() => {
-      render(<Provider store={store}> <RocketPage /> </Provider>, container);
+      render(
+        <Provider store={store}>
+          <RocketPage />
+        </Provider>, container,
+      );
     });
 
     expect(pretty(container.innerHTML)).toMatchSnapshot();
-  })
-})
+  });
+});

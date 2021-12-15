0e0fbb34a04d49e74f94140c71e3cd3bc1d83c5b
unknown <okpara_tochukwu@yahoo.com>
2021-12-15 10:41:44 +0100

    removed j-center from styling

--- src/component/pages/DragonPage/ProfileDragon.js
+++ src/component/pages/DragonPage/ProfileDragon.js
@@ -5,7 +5,7 @@ const Profilerocket = () => {
   const dragonDetail = useSelector((state) => state.dragonReducer);
   const reservedDragon = dragonDetail.filter((dragon) => dragon.reserved === true);
   return (
-    <div className="profile-rocket d-flex f-col j-center">
+    <div className="profile-rocket d-flex f-col">
       <h1 className="rocket-profile-header">My Dragon</h1>
       <article className="profile-rocket-cont">
         {reservedDragon.map((dragon) => <p className="rocket-profile-name" key={dragon.id}>{dragon.name}</p>)}
